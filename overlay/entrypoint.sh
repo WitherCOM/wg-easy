d484d5a8126e74c17d34e5abb4b2aa6e5eb8d1bf
Németh Csaba Tibor <nemeth.csaba.tibor@gmail.com>
2024-09-15 21:41:23 +0200

    Fix entrypoint.sh

--- overlay/entrypoint.sh
+++ overlay/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Replace keys to envs
-sed -i "s/<address>/${ADDRESS}/g" /etc/wireguard/wg0.conf
-sed -i "s/<private key>/${PRIVATE_KEY}/g" /etc/wireguard/wg0.conf
-sed -i "s/<allowed cidr>/${ALLOWED_CIDR}/g" /etc/wireguard/wg0.conf
-sed -i "s/<public key>/${PUBLIC_KEY}/g" /etc/wireguard/wg0.conf
-sed -i "s/<endpoint>/${ENDPOINT}/g" /etc/wireguard/wg0.conf
+sed -i "s|<address>|${ADDRESS}|g" /etc/wireguard/wg0.conf
+sed -i "s|<private key>|${PRIVATE_KEY}|g" /etc/wireguard/wg0.conf
+sed -i "s|<allowed cidr>|${ALLOWED_CIDR}|g" /etc/wireguard/wg0.conf
+sed -i "s|<public key>|${PUBLIC_KEY}|g" /etc/wireguard/wg0.conf
+sed -i "s|<endpoint>|${ENDPOINT}|g" /etc/wireguard/wg0.conf
 
 wg-quick up wg0
 
